5df19ee237e9719982db6e3d41768073ae95e968
manish-manghwani <manghwani.manish1996@gmail.com>
2020-06-25 02:06:14 +0530

    removed unused code. added License and description

--- src/SpreadSheetServiceProvider.php
+++ src/SpreadSheetServiceProvider.php
@@ -10,18 +10,10 @@ class SpreadSheetServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/spreadshet.php' => config_path('spreadshet.php'),
-            ], 'config');
-
+            
             $this->commands([
                 SpreadSheetCommand::class,
             ]);
         }
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/spreadsheet.php', 'spreadsheet');
     }
 }
